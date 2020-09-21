e5452671907dc18aa36464f9c6abc82cfc5c6099
TsapovAndrei <Landcate@mail.ru>
2020-09-21 20:49:17 +0300

    feature/add-api-properties

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
-
-const API_KEY = 'AIzaSyCqx5Nrt2hQb0Lnq6Z8LIh0V0Q26CP_0HU';
+import data from './properties.json'
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         maxResults: 5,
-        key: API_KEY
+        key: data.api_key
     }
 });
